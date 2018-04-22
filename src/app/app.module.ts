445f8d54243ee1cef10ca16aa7941156505c88ed
Aldous Nicolas <aldous.darcy@live.com>
2018-04-22 11:48:35 +0800

    Initial commit with pwa and sw

---
+++ src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { ServiceWorkerModule } from '@angular/service-worker';
+
+import { AppComponent } from './app.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    ServiceWorkerModule.register('/ngsw-worker.js', {enabled: true}),
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
